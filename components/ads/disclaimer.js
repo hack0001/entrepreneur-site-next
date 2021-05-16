e0b10621718054bbd2f568da3e1682ac651f7de2
Thomas Patel <tompatelwales@gmail.com>
2021-05-16 13:52:21 +0100

    adding data no snippet

--- components/ads/disclaimer.js
+++ components/ads/disclaimer.js
@@ -3,7 +3,7 @@ import CustomLink from "@components/Link/customLink";
 
 const Disclaimer = ({ queryLinkCheck, query }) => {
 	return (
-		<div className={styles.disclaimer}>
+		<div className={styles.disclaimer} data-nosnippet>
 			***Disclosure: This post may contain affiliate links, meaning we recieve a
 			commission if you decide to make a purchase via one of our links, at no
 			extra cost to you. Please read our full disclosure&nbsp;
